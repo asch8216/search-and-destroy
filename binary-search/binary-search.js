7cf04ca0dc3aaad1a1ca5dab871784a4b0804802
Edward Lu <e.lu616@gmail.com>
2019-10-23 15:20:13 -0400

    finished binary-searh

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,9 +1,34 @@
-'use strict';
+'use strict'
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
-};
+  let middle = Math.floor(array.length / 2)
+  if (array[middle] === target) return true
+
+  if (array.length === 1 && array[0] !== target) {
+    return false
+  }
+
+  if (array[middle] < target) {
+    return binarySearch(array.slice(middle), target)
+  } else if (array[middle] > target) {
+    return binarySearch(array.slice(0, middle), target)
+  }
+
+  // let middle = Math.floor(array.length / 2)
+
+  // while (array[middle] !== target) {
+  //   if (middle === 0 || middle === array.length - 1) {
+  //     return false
+  //   } else if (target < array[middle]) {
+  //     middle = Math.floor(middle / 2)
+  //   } else if (target > array[middle]) {
+  //     middle = Math.floor((middle + array.length) / 2)
+  //   }
+  // }
+
+  // return true
+}
 
 /*
 	EXTRA CREDIT:
